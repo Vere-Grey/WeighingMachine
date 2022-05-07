3407014caf4dcbd11fcd346bc2695a42810656d3
cihlar <martin.cihlar@ysoft.com>
2022-05-07 12:41:55 +0200

    refactoring

--- WeighingMachine.cs
+++ WeighingMachine.cs
@@ -3,43 +3,35 @@ using System;
 class WeighingMachine
 {
     private int precision;
-
-    public int Precision
-    {
-        get => precision;
-        set
-        {
-            if (value < 0)
-            {
-                throw new ArgumentOutOfRangeException($"Precision cannot be negative {value}.");
-            }
-            precision = value;
-        }
-    }
+    private double weight;
+    private double tareAdjustment = 5;
 
     public WeighingMachine(int precision)
     {
         Precision = precision;
     }
 
-    private double weight;
+    public int Precision
+    {
+        get => precision;
+        set
+        {
+            if (value < 0) throw new ArgumentOutOfRangeException($"Precision cannot be negative {value}.");
+            precision = value;
+        }
+    }
 
     public double Weight
     {
         get => weight;
         set
         {
-            if (value < 0)
-            {
-                throw new ArgumentOutOfRangeException($"Weight cannot be negative {value}.");
-            }
+            if (value < 0) throw new ArgumentOutOfRangeException($"Weight cannot be negative {value}.");
             weight = value;
         }
     }
 
     public string DisplayWeight => $"{(Weight - TareAdjustment).ToString($"f{Precision}")} kg";
-
-    private double tareAdjustment = 5;
 
     public double TareAdjustment
     {
